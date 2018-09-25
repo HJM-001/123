452f201eea456550742401488faeb5a53e32b2eb
HJM <HJM@HJM001-WIN7SP1->
2018-09-25 10:16:42 +0800

    添加项目文件。

---
+++ 03_view_range_image/stdafx.h
@@ -0,0 +1,26 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+
+// TODO: 在此处引用程序需要的其他头文件
+#include "use_pcl_lib.h"
+#include "use_vtk_lib.h"
+#include <pcl/ModelCoefficients.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
---
+++ 04_narf_keypoint_extraction/stdafx.h
@@ -0,0 +1,25 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+
+// TODO: 在此处引用程序需要的其他头文件
+#include "use_pcl_lib.h"
+#include "use_vtk_lib.h"
+#include <boost/thread/thread.hpp>
+#include <pcl/range_image/range_image.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/visualization/range_image_visualizer.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/features/range_image_border_extractor.h>
+#include <pcl/keypoints/narf_keypoint.h>
+#include <pcl/console/parse.h>
